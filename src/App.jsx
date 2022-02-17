dc4b1bcd05e50f5ff281bf4e0e775e6c5ec7bf41
Federico Marazzani <marazzani.dev@gmail.com>
2022-02-17 13:19:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import GlobalStyle from './util/GlobalStyle';
+
+const App = () => {
+  return (
+    <>
+    <GlobalStyle />
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+
+
+export default App
