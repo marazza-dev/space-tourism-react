6100b05afffbfee1a65afbc7012ed5c9736ba14b
Federico Marazzani <marazzani.dev@gmail.com>
2022-02-17 18:44:29 +0100

    setup Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import { Routes, Route } from "react-router-dom";
 import { Home, Destination, Technology, Crew } from "./pages";
 import { theme } from "./util/theme.js";
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/destination' element={<Destination />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavContainer = styled.nav`
+  width: 100%;
+  height: 80px;
+  border: 1px solid red;
+`;
+
+const Navbar = () => {
+  return <NavContainer>testo</NavContainer>;
+};
+
+export default Navbar;
