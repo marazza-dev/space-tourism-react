882ce846c8c259d737cd761baa944e94ada7d849
Federico Marazzani <marazzani.dev@gmail.com>
2022-02-17 16:20:46 +0100

    setup router and page folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import GlobalStyle from './util/GlobalStyle';
+import React from "react";
+import GlobalStyle from "./util/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { Routes, Route } from "react-router-dom";
+import { Home, Destination, Technology, Crew } from "./pages";
+import { theme } from "./util/theme.js";
 
-const App = () => {
+function App() {
   return (
     <>
-    <GlobalStyle />
-      <h1>Hello</h1>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/destination' element={<Destination />} />
+          <Route path='/technology' element={<Technology />} />
+          <Route path='/crew' element={<Crew />} />
+        </Routes>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home/Home.jsx";
+export { default as Destination } from "./Destination/Destination.jsx";
+export { default as Technology } from "./Technology/Technology.jsx";
+export { default as Crew } from "./Crew/Crew.jsx";
