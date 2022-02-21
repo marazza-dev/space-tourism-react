e673bb45b70757dcb1e9a595a0133a17845fddf2
Federico Marazzani <marazzani.dev@gmail.com>
2022-02-21 18:11:39 +0100

    add background functionality :: create BackgroundWrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import { Routes, Route } from "react-router-dom";
 import { Home, Destination, Technology, Crew } from "./pages";
 import { theme } from "./util/theme.js";
 import Navbar from "./components/Navbar";
+import BackgroundWrapper from "./components/BackgroundWrapper";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/destination' element={<Destination />} />
-          <Route path='/technology' element={<Technology />} />
-          <Route path='/crew' element={<Crew />} />
-        </Routes>
+        <BackgroundWrapper>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/destination' element={<Destination />} />
+            <Route path='/technology' element={<Technology />} />
+            <Route path='/crew' element={<Crew />} />
+          </Routes>
+        </BackgroundWrapper>
       </ThemeProvider>
     </>
   );
