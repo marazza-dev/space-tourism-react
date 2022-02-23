bd3a27150c6ff445c8589bade1576297a0133c55
Federico Marazzani <marazzani.dev@gmail.com>
2022-02-23 15:41:44 +0100

    restart Destination page :: implement page in a different way

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -1,37 +1,7 @@
 import React from "react";
-import styled from "styled-components";
-import DestinationTab from "../../components/DestinationTab";
-
-const MainContainer = styled.main``;
-const MainHeading = styled.h1`
-  color: ${(props) => props.theme.clrLight};
-  font-size: 1rem;
-  font-family: ${(props) => props.theme.ffSansCondens};
-  letter-spacing: 2.7px;
-  text-transform: uppercase;
-
-  @media (min-width: ${(props) => props.theme.brSmall}) {
-    font-size: 1.125rem;
-    letter-spacing: 3.38px;
-    margin: 0 auto;
-  }
-  @media (min-width: ${(props) => props.theme.brMedium}) {
-    font-size: ${(props) => props.theme.fs500};
-    letter-spacing: 4.75px;
-    margin: 0;
-  }
-`;
-const MainSpan = styled.span``;
 
 const Destination = () => {
-  return (
-    <MainContainer>
-      <MainHeading>
-        <MainSpan>01</MainSpan> Pick your destination
-      </MainHeading>
-      <DestinationTab />
-    </MainContainer>
-  );
+  return <div>Destination</div>;
 };
 
 export default Destination;
