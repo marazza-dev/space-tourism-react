e6a5b459429620f029797c766dc729f3857521d9
Federico Marazzani <marazzani.dev@gmail.com>
2022-03-05 11:41:13 +0100

    update App :: add basename to router

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/space-tourism-react'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
