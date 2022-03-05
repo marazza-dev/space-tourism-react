5be23d265041c8e8aecbac246f5632a4d690c6ba
Federico Marazzani <marazzani.dev@gmail.com>
2022-03-05 08:19:51 +0100

    update technology page :: add styles at large vw

--- src/pages/Technology/Technology.jsx
+++ src/pages/Technology/Technology.jsx
@@ -18,7 +18,6 @@ const techs = [
       "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operations. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch",
     image_portrait: launchPortrait,
     image_landscape: launchLandscape,
-    number: 1,
   },
   {
     title: "SPACEPORT",
@@ -26,7 +25,6 @@ const techs = [
       "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
     image_portrait: spaceportPortrait,
     image_landscape: spaceportLandscape,
-    number: 2,
   },
   {
     title: "SPACE CAPSULE",
@@ -34,9 +32,17 @@ const techs = [
       "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
     image_portrait: capsulePortrait,
     image_landscape: capsuleLandscape,
-    number: 3,
   },
 ];
+
+const Title = styled(MainTitle)`
+  @media (min-width: ${(props) => props.theme.brSmall}) {
+    margin-left: 2rem;
+  }
+  @media (min-width: ${(props) => props.theme.brMedium}) {
+    margin: 0;
+  }
+`;
 
 const TechMain = styled.main`
   display: grid;
@@ -53,15 +59,34 @@ const TechMain = styled.main`
   & > img {
     grid-area: image;
     margin-top: 2rem;
+    @media (min-width: ${(props) => props.theme.brSmall}) {
+      margin-top: 3rem;
+    }
+  }
+  @media (min-width: ${(props) => props.theme.brMedium}) {
+    text-align: left;
+    grid-template-areas:
+      ". title title ."
+      ". tabs details image"
+      ". tabs details image";
+    column-gap: 4rem;
+    justify-items: start;
+    padding-left: 5rem;
   }
 `;
 const NumberList = styled.div`
   grid-area: tabs;
-  width: 50%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-block: 1.5rem;
+  gap: max(1rem, 3vw);
+  @media (min-width: ${(props) => props.theme.brSmall}) {
+    margin-block: 3rem;
+  }
+  @media (min-width: ${(props) => props.theme.brMedium}) {
+    flex-direction: column;
+  }
 `;
 const NumberItem = styled.button`
   width: 3rem;
@@ -83,6 +108,9 @@ const NumberItem = styled.button`
   &:focus {
     border: 2px solid hsl(0 0% 100% / 0.75);
   }
+  @media (min-width: ${(props) => props.theme.brMedium}) {
+    width: 4rem;
+  }
 `;
 const TechContent = styled.article`
   grid-area: details;
@@ -102,12 +130,21 @@ const TechContent = styled.article`
       font-family: ${(props) => props.theme.ffSerif};
       font-size: 1.5rem;
       color: ${(props) => props.theme.clrWhite};
+      @media (min-width: ${(props) => props.theme.brSmall}) {
+        font-size: 2.5rem;
+      }
     }
   }
 
   & p {
-    padding-inline: 2rem;
+    padding-inline: 1rem;
     line-height: 1.85;
+    @media (min-width: ${(props) => props.theme.brSmall}) {
+      padding-inline: 8rem;
+    }
+    @media (min-width: ${(props) => props.theme.brMedium}) {
+      padding: 0;
+    }
   }
 `;
 
@@ -125,9 +162,9 @@ const Technology = () => {
   return (
     <>
       <TechMain>
-        <MainTitle>
+        <Title>
           <span>03</span> space launch 101
-        </MainTitle>
+        </Title>
         <img
           src={
             detectDesktop
