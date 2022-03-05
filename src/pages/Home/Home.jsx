7e0d6a0f9f73b1489ab6b333d45fe35417a04307
Federico Marazzani <marazzani.dev@gmail.com>
2022-03-05 08:59:48 +0100

    refactor home page button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import LargeButton from "../../components/LargeButton";
+
 const StyledMain = styled.main`
   display: grid;
   place-items: center;
@@ -60,44 +62,6 @@ const StyledSpan = styled.span`
   color: ${(props) => props.theme.clrWhite};
 `;
 
-const LargeButton = styled.a`
-  display: inline-grid;
-  position: relative;
-  place-items: center;
-  padding: 0 2em;
-  aspect-ratio: 1;
-  border-radius: 50%;
-  text-decoration: none;
-  z-index: 1;
-  font-size: 1.5rem;
-  text-transform: uppercase;
-  font-family: ${(props) => props.theme.ffSerif};
-  color: ${(props) => props.theme.clrDark};
-  background-color: ${(props) => props.theme.clrWhite};
-
-  &::after {
-    content: "";
-    position: absolute;
-    z-index: -1;
-    width: 100%;
-    height: 100%;
-    background: hsl(0 0% 100% / 0.1);
-    border-radius: 50%;
-    opacity: 0;
-    transition: opacity 500ms linear, transform 750ms ease-in-out;
-  }
-
-  &:hover::after,
-  &:focus::after {
-    opacity: 1;
-    transform: scale(1.5);
-  }
-
-  @media (min-width: ${(props) => props.theme.brMedium}) {
-    font-size: 2rem;
-  }
-`;
-
 const Home = () => {
   return (
     <>
@@ -114,7 +78,7 @@ const Home = () => {
           </p>
         </div>
         <div>
-          <LargeButton href='#'>Explore</LargeButton>
+          <LargeButton to='/destination' text='Explore'></LargeButton>
         </div>
       </StyledMain>
     </>
