749b24659724d31836e6c5f580879ba0c3c84ac2
Federico Marazzani <marazzani.dev@gmail.com>
2022-03-05 11:42:41 +0100

    add dist for deploy

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home/Home.jsx";
-export { default as Destination } from "./Destination/Destination.jsx";
-export { default as Technology } from "./Technology/Technology.jsx";
-export { default as Crew } from "./Crew/Crew.jsx";
-export { default as NotFound } from "./NotFound/NotFound.jsx";
